bf152a88abf276b8f6d716c47f62f070bff1e5a4
Will Cober <wcober@revshop.com>
2017-11-27 08:20:39 -0500

    Minor formating fixes.

--- perl/PQS/model/categories.pm
+++ perl/PQS/model/categories.pm
@@ -50,9 +50,9 @@ sub get_children_from_id {
 
   my $list;
   if ( $id ) {
-  	$list = $dbh->selectcol_arrayref("select id from categories where parent = ?  ORDER BY name ", undef, $id);
+  	$list = $dbh->selectcol_arrayref("select id from categories where parent = ? AND active  ORDER BY name ", undef, $id);
    } else {
-  	$list = $dbh->selectcol_arrayref("select id from categories where parent is NULL  ORDER BY name ");
+  	$list = $dbh->selectcol_arrayref("select id from categories where parent is NULL AND active  ORDER BY name ");
   }
 }
 
