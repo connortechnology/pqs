a7fae7a7a292366596bde5291fbcde3d6c547198
heyruyn23 <hkim@246.dev.print-quotes-software.com>
2020-05-21 14:08:00 -0400

    in Bindery tab, Prepress is not showing anymore

--- perl/eprint/docket.pm
+++ perl/eprint/docket.pm
@@ -2385,14 +2385,12 @@ sub setup_docket {
 					my ($ref, $name, $id, $supplied, $desc) 
 						= @$service{qw(ref name id supplied desc)};
 
-						my ($data, $material) =
-							summary($r, $log, $dbh, $pid, $id, $qtyIndex, $name, \$form_count);
-
-				
-				for my $m (@{$material}) {
-					push @materials, $m;
-				}
-			}
+                    my ($data, $material) =
+                        summary($r, $log, $dbh, $pid, $id, $qtyIndex, $name, \$form_count);
+                    for my $m (@{$material}) {
+                        push @materials, $m;
+                    }
+			    }
 		  }
 
 
@@ -2460,6 +2458,7 @@ sub setup_docket {
                       AND lngprojectindex =?
                 }, undef, $id, $pid);
 
+                next if ($ref eq 'impositionlayout' && $catID == -3);
 				# Imposition Comments are stored with the project.
 				if ( $ref eq 'impositionlayout' ) {
 					$comment = $dbh->selectrow_array(q{
@@ -2542,7 +2541,6 @@ sub setup_docket {
                 }
                 $variable->{dockethash}{$name}{comment} = $comment;
             }
-
             push @categories,
               { id                    => $cat,
                 name                  => $cat,
