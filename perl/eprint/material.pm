10fbfeea5005fafdc20b3aebe510d2339a2e8fa4
Isaac Connor <iconnor@connortechnology.com>
2024-11-15 17:06:12 -0500

    Don't try to load material info if none is specified, instead log where it was called from

--- perl/eprint/material.pm
+++ perl/eprint/material.pm
@@ -6,63 +6,65 @@ use eprint::customer qw(get_discount);
 use eprint::equipment;
 
 sub get_price {
-    my ($log, $dbh, $variable, $material, $range, $equip) = @_;
+  my ($log, $dbh, $variable, $material, $range, $equip) = @_;
 
-    my ($clause, $mi, @args);
+  my ($clause, $mi, @args);
 
-    if ( $material =~ /^\d+$/ ) {
-        # Verify the numberic ID
-        $mi = scalar $dbh->selectrow_array(q{
-                    SELECT lngindex FROM tbl_materials WHERE lngindex = ?
-        }, undef, $material);
-    }
+  if (!$material) {
+    my ( $caller, undef, $line ) = caller;
+    print STDERR "Empty material passed to get_price from $caller:$line\n";
+    return;
+  }
 
+  if ($material =~ /^\d+$/) {
+    # Verify the numberic ID
+    $mi = scalar $dbh->selectrow_array(q{SELECT lngindex FROM tbl_materials WHERE lngindex = ?}, undef, $material);
+  } else {
     # Fetch the numberic ID if they're passed us a string.
-    $mi = scalar $dbh->selectrow_array(q{
-        SELECT lngindex FROM tbl_materials WHERE strid = ?
-    }, undef, $material) unless $mi;
+    $mi = scalar $dbh->selectrow_array(q{SELECT lngindex FROM tbl_materials WHERE strid = ?}, undef, $material);
+  } # end if id or string
 
-    if (defined $equip && $equip) {
-        # Fetch the numberic ID if they're passed us a string.
-        unless ($equip =~ /^\d+$/) {
-            $equip = eprint::equipment::get_index_by_id($log, $dbh, $equip)
-                or die "Invalid equipment in material price lookup";
-        }
-
-        $clause .= q{ AND (lngequipmentindex = ? OR lngequipmentindex IS NULL) };
-        push @args, $equip;
+  if (defined($equip) && $equip) {
+    # Fetch the numberic ID if they're passed us a string.
+    unless ($equip =~ /^\d+$/) {
+      $equip = eprint::equipment::get_index_by_id($log, $dbh, $equip)
+        or die "Invalid equipment in material price lookup";
     }
 
-    # If a value is supplied check it's in the range Note: ranges values are
-    # neither validated nor constrained so you can get 'interesting' results. 
-    if (defined $range && $range ne '' && $range >= 0) {
-        $range = int $range;
+    $clause .= q{ AND (lngequipmentindex = ? OR lngequipmentindex IS NULL) };
+    push @args, $equip;
+  }
 
-        $clause .= q{ 
-            AND (? >= lngmin OR lngmin IS NULL) 
-            AND (? <= lngmax OR lngmax IS NULL)
-        };
-        push @args, $range, $range;
-    }
-    my ($price, $discountable) = $dbh->selectrow_array(qq{ 
-        SELECT dblprice, (CASE WHEN ysndiscountable = 'Y' THEN 1 ELSE 0 END)
-        FROM tbl_material_prices m, tbl_customer c
-        WHERE m.lnglistindex     = c.lngpricelist
-          AND m.lngmaterialindex = ?
-          AND c.lngcustomerid    = ?
-          $clause
-        ORDER BY lngequipmentindex IS NULL, lngmin
-        LIMIT 1
+  # If a value is supplied check it's in the range Note: ranges values are
+  # neither validated nor constrained so you can get 'interesting' results. 
+  if (defined $range && $range ne '' && $range >= 0) {
+    $range = int $range;
+
+    $clause .= q{ 
+    AND (? >= lngmin OR lngmin IS NULL) 
+    AND (? <= lngmax OR lngmax IS NULL)
+    };
+    push @args, $range, $range;
+  }
+  my ($price, $discountable) = $dbh->selectrow_array(qq{ 
+    SELECT dblprice, (CASE WHEN ysndiscountable = 'Y' THEN 1 ELSE 0 END)
+    FROM tbl_material_prices m, tbl_customer c
+    WHERE m.lnglistindex     = c.lngpricelist
+    AND m.lngmaterialindex = ?
+    AND c.lngcustomerid    = ?
+    $clause
+    ORDER BY lngequipmentindex IS NULL, lngmin
+    LIMIT 1
     }, undef, $mi, $variable->{cust_id}, @args);
 
-    # Apply any pricelist or customer discounts if applicable.
-    if ($discountable) {
-        my $discount = get_discount($dbh, $variable->{cust_id});
+  # Apply any pricelist or customer discounts if applicable.
+  if ($discountable) {
+    my $discount = get_discount($dbh, $variable->{cust_id});
 
-        $price *= 1 + ($discount/100) if $discount;
-    }
+    $price *= 1 + ($discount/100) if $discount;
+  }
 
-    return $price;
+  return $price;
 }
 	
 sub get_name {
