bb966d3d1f9cc60d093fcc89b0a3592654f1da09
Isaac Connor <iconnor@connortechnology.com>
2024-12-31 17:49:33 -0500

    Add calcs when custom stock changes, rename doublesided. Add remaining details when passing to admin side

--- www/javascripts/proj/service/printing/paper.js
+++ www/javascripts/proj/service/printing/paper.js
@@ -1,14 +1,14 @@
 // Fields (names) needed for stock lookup.
 var STOCK_ARGS = [
-    'pid',
-    'sid',
-    'template',
-    'outdoor_printing',
-	'press',
-    'stock_name',
-    'stock_finish',
-    'stock_colour',
-    'stock_weight',
+  'pid',
+  'sid',
+  'template',
+  'outdoor_printing',
+  'press',
+  'stock_name',
+  'stock_finish',
+  'stock_colour',
+  'stock_weight',
 ];
 
 // Bind the stock lookup and selection events to the needed elements.
@@ -47,166 +47,163 @@ Event.observe(window, 'load', function (e) {
 	// Get new paper list on press override.
 	Event.observe($('override_press'),   'click', stock_lookup);
 
-    return true;
+  return true;
 });
 
-
 function stock_bind_option (e) {
-    // The index of the currently selected option.
-    var selected = this.selectedIndex;
+  // The index of the currently selected option.
+  var selected = this.selectedIndex;
 
-    // Remove all options except the selected one (in reverse order so
-    // indexing doesn't shift).
-    for (var i = this.options.length; i >= 0; i--) {
-        if (i != selected) this.options[i] = null;
-    }
+  // Remove all options except the selected one (in reverse order so
+  // indexing doesn't shift).
+  for (var i = this.options.length; i >= 0; i--) {
+    if (i != selected) this.options[i] = null;
+  }
 
-    // Add an empty value so the user can unbind the selection.
-    this.options[1] = new Option('Clear', '');
+  // Add an empty value so the user can unbind the selection.
+  this.options[1] = new Option('Clear', '');
 
-    return true;
+  return true;
 }
 
 function stock_lookup (e) {
-    var form = $('f1');
+  var form = $('f1');
 
-    // Get the value of each of the named elements.
-    var values = STOCK_ARGS.map(function (name) {
-        var obj = form[name];
-        
-        if (!obj) return '';
+  // Get the value of each of the named elements.
+  var values = STOCK_ARGS.map(function (name) {
+    var obj = form[name];
 
-		// Only send press id if override is checked.
-		if (name == 'press' && ! form['override_press'].checked) return '';
+    if (!obj) return '';
 
-        // If the form object is a NodeList (radio button group) we'll get the
-        // first selected elem.
-        if (obj.nodeType != 1) {
-            obj = $A(obj).detect(function (x) { return $F(x) });
-            if (!obj) return;
-        }
+    // Only send press id if override is checked.
+    if (name == 'press' && ! form['override_press'].checked) return '';
 
-        return $F(obj); // Form field value
-    });
+    // If the form object is a NodeList (radio button group) we'll get the
+    // first selected elem.
+    if (obj.nodeType != 1) {
+      obj = $A(obj).detect(function (x) { return $F(x) });
+      if (!obj) return;
+    }
 
-    // TODO disable the fields in question and prevent calculations until the
-    // call returns or a timeout occurs.
+    return $F(obj); // Form field value
+  });
 
-    // TODO Replace JSRS call with an Ajax request once the server side
-    // handler is in place.
+  // TODO disable the fields in question and prevent calculations until the
+  // call returns or a timeout occurs.
 
-    // Make an async call to find all valid options for undefined substrate
-    // attributes (will be populated by the callback).
-    jsrsExecute(
-        '/jsrs', stock_results, 'eprint::paper::substrate_lookup', values
-    );
+  // TODO Replace JSRS call with an Ajax request once the server side
+  // handler is in place.
 
-    return true;
+  // Make an async call to find all valid options for undefined substrate
+  // attributes (will be populated by the callback).
+  jsrsExecute(
+    '/jsrs', stock_results, 'eprint::paper::substrate_lookup', values
+  );
+
+  return true;
 }
 
 // Takes a JSON serialised hash (object), each key representing a stock
 // attribute name and the value a list of [label, value] pairs that are used
 // to populate the stock attribute select boxes.
 function stock_results (str) {
-    // if (str == '{}') { stock_unbind_all(); return false; } // Invalid paper
+  // if (str == '{}') { stock_unbind_all(); return false; } // Invalid paper
 
-    var json = eval('(' + str + ')');
+  var json = eval('(' + str + ')');
 
-    for (var name in json) {
-        var select = $('stock_' + name);
+  for (var name in json) {
+    var select = $('stock_' + name);
 
-        clear_select(select);
-        stock_populate_select(select, json[name]);
+    clear_select(select);
+    stock_populate_select(select, json[name]);
+  }
+
+
+  var form  = $('f1');
+  // If the page wants to display stock details, check if all stock
+  // selections have a value fetching the details if they do.
+  var details = $('stock_details');
+  var underbase = $('underbase_discharge');
+  if (details || underbase) {
+    // Get the name, finish, colour, and weight.
+    var attrs = $A(['name', 'finish', 'colour', 'weight']).map(
+      function (name) { return $F(form['stock_' + name]) }
+    );
+
+    // If all are filled out find the details for that stock.
+    if (attrs.all(function (x) { return x })) {
+      jsrsExecute(
+        '/jsrs', 
+        function (str, x) { 
+          var resp = eval('(' + str + ')');
+          if ( resp.underbase == 1 ) {
+            underbase.checked = true;
+            mySelect3($('underbase_discharge'));
+          } else {
+            $('underbase_none').checked = true;
+            mySelect3($('underbase_discharge'));
+          }
+
+          details.display(!!resp.details);
+
+          if (resp.details)
+            details.lastChild.innerHTML = resp.details;
+        },
+        'eprint::paper::substrate_details', 
+        attrs
+      );
+    } else { 
+      details.display(false);
+      details.lastChild.innerHTML = ''; 
     }
-
-
-    // If the page wants to display stock details, check if all stock
-    // selections have a value fetching the details if they do.
-    var details = $('stock_details');
-    var underbase = $('underbase_discharge');
-    if (details || underbase) {
-        // Get the name, finish, colour, and weight.
-        var form  = $('f1');
-        var attrs = $A(['name', 'finish', 'colour', 'weight']).map(
-            function (name) { return $F(form['stock_' + name]) }
-        );
-
-        // If all are filled out find the details for that stock.
-        if (attrs.all(function (x) { return x })) {
-            jsrsExecute(
-                '/jsrs', 
-                function (str, x) { 
-                    var resp = eval('(' + str + ')');
-		    if ( resp.underbase == 1 ) {
-			underbase.checked = true;
-			mySelect3($('underbase_discharge'));
-		    } else {
-			$('underbase_none').checked = true;
-			mySelect3($('underbase_discharge'));
-		    }
-
-                    details.display(!!resp.details);
-
-                    if (resp.details)
-                        details.lastChild.innerHTML = resp.details;
-                },
-                'eprint::paper::substrate_details', 
-                attrs
-            );
-        }
-        else { 
-            details.display(false);
-            details.lastChild.innerHTML = ''; 
-        }
-    }
-
-    return true;
+  }
+  calc();
+  return true;
 }
 
 
 // Given a select object and an array of [label, value] pairs append each
 // tuple as an options to the select box.
 function stock_populate_select (select, options) {
-    if (!select || select.tagName.toLowerCase() != 'select') return false;
+  if (!select || select.tagName.toLowerCase() != 'select') return false;
 
-    select[select.length] = new Option('Please select one' , '');
+  select[select.length] = new Option('Please select one' , '');
 
-    for (var i = 0; i < options.length; i++) {
-        select[select.length] = new Option(options[i], options[i]);
-    }
+  for (var i = 0; i < options.length; i++) {
+    select[select.length] = new Option(options[i], options[i]);
+  }
 
-    // If there's only a single valid option available, we'll bind to it.
-    if (select.options.length == 2) {
-        select.selectedIndex = 1;
-        stock_bind_option.apply(select);
-    }
+  // If there's only a single valid option available, we'll bind to it.
+  if (select.options.length == 2) {
+    select.selectedIndex = 1;
+    stock_bind_option.apply(select);
+  }
 
-    return true;
+  return true;
 }
-
 
 // Clear given select options.
 function clear_select (select) {
-    if (!select || select.tagName.toLowerCase() != 'select') return false;
+  if (!select || select.tagName.toLowerCase() != 'select') return false;
 
-    while (select.length > 0) { select.options[0] = null; }
+  while (select.length > 0) { select.options[0] = null; }
 
-    return true;
+  return true;
 }
 
 // Unbinds all currect stock filters and re-populates the stock selection.
 function stock_unbind_all (e) {
-    for (var i = 0; i < STOCK_ARGS.length; i++) {
-        var elem = document.getElementById(STOCK_ARGS[i]);
+  for (var i = 0; i < STOCK_ARGS.length; i++) {
+    var elem = document.getElementById(STOCK_ARGS[i]);
 
-        if ( elem && elem.tagName.toLowerCase() == 'select') {
-			if (elem.id == 'press') continue;
-            clear_select(elem);
-        }
+    if ( elem && elem.tagName.toLowerCase() == 'select') {
+      if (elem.id == 'press') continue;
+      clear_select(elem);
     }
-    stock_lookup();
+  }
+  stock_lookup();
 
-    return true;
+  return true;
 }
 
 function paper_price_calc( element, group ) {
@@ -246,10 +243,8 @@ function paper_price_calc( element, group ) {
     form.elements['StockPricePerM'+group].value = do_decimals( costcwt * form.elements['txtCustomMWeight'+group].value / 100, 2 );
     console.log("New value", ( costcwt * form.elements['txtCustomMWeight'+group].value / 100));
   } // end if
+  calc();
 } // end function
-
-function calc_basis_weight_from_weight(weight_element) {
-}
 
 function mweight_to_gsm( form, signature ) {
   const width = parseFloat(1*form.elements['txtSpecificStockWidth'+signature].value);
@@ -278,7 +273,7 @@ function basis_weight_to_gsm( form, signature ) {
   var basis_weight = parseFloat(1*form.elements['basis_mweight'+signature].value);
   if (basis_weight) {
     mweight = (width*height) * basis_weight / (basis_width*basis_height);
-    form.elements['txtCustomMWeight'+signature].value = mweight;
+    form.elements['txtCustomMWeight'+signature].value = Math.round(mweight*10)/10;
     // mweight is the weight of 1000 sheets, so calc the wpsi and multiply by 703064.5 to get gsm
     //console.log( "wpsi: " + (mweight/1000)/(width*height) );
     var gsm = Math.round((mweight/1000)/(width*height)*70306450)/100;
@@ -333,6 +328,7 @@ function type_onchange(radio) {
     $j('#sheets_per_package'+signature).innerHTML='lbs';
   }
   update_basis_size(radio);
+  calc();
 }
 
 function update_basis_size(element) {
@@ -366,7 +362,7 @@ function specific_stock_onchange(radio) {
   const re = /^rdbSpecificStock(\d*)$/;
   const matches = radio.name.match(re);
   const signature = matches[1];
-  if(radio.value=='Y'){
+  if (radio.value=='Y'){
     $j('#HouseStock'+signature).hide();
     $j('#SpecificStock'+signature).show();
     if(radio.form.ddmStockSheetSize1){
@@ -381,20 +377,23 @@ function specific_stock_onchange(radio) {
   } else {
     $j('#HouseStock'+signature).show();
     $j('#SpecificStock'+signature).hide();
-    Stock_onchange(radio, signature);
+    //Stock_onchange(radio, signature);
   }
+  calc();
 }
 
 function brand_onchange(element) {
   update_doublesided();
   update_basis_size(element);
   select_grade();
+  calc();
 }
 
 function finish_onchange(element) {
   update_doublesided();
   update_basis_size(element);
   select_grade();
+  calc();
 }
 
 function weight_onchange(element) {
@@ -408,7 +407,7 @@ function weight_onchange(element) {
     const weight = matches[1];
     console.log(signature, weight);
     form.elements['basis_mweight'+signature].value = weight * 2;
-    mweight_to_gsm(form, signature);
+    basis_weight_to_gsm(form, signature);
   } else {
     re = /([\d\.])PT/i;
     if (matches = re.exec(element.value)) {
@@ -418,6 +417,7 @@ function weight_onchange(element) {
       console.log("No match against "+element.value);
     }
   }
+  calc();
 }
 
 function update_doublesided() {
@@ -425,11 +425,11 @@ function update_doublesided() {
   if (form.elements['txtSpecificStockFinish'].value.match(/C1S/i)
     || form.elements['txtSpecificStockFinish'].value.match(/1 ?Side/i)
   ) {
-    set_rdb_value(form.elements['CustomSheetDoubleSided'], 'N');
+    set_rdb_value(form.elements['doublesided'], 'N');
   } else if (form.elements['txtSpecificStockFinish'].value.match(/C2S/i)
     || form.elements['txtSpecificStockFinish'].value.match(/2 ?Side/i)
   ) {
-    set_rdb_value(form.elements['CustomSheetDoubleSided'], 'Y');
+    set_rdb_value(form.elements['doublesided'], 'Y');
   }
 }
 
@@ -442,6 +442,7 @@ function calliper_onchange(element) {
   } else {
     console.log("No match");
   }
+  calc();
 }
 
 function select_grade() {
@@ -496,6 +497,10 @@ function addstock(button) {
               +'&'+'basis_height='+encodeURIComponent(form.elements['basis_height'].value)
               +'&'+'gsm='+encodeURIComponent(form.elements['txtStockGSM'].value)
               +'&'+'grade='+encodeURIComponent(get_value(form.elements['StockGrade']))
+              +'&'+'sheets_per_package='+encodeURIComponent(get_value(form.elements['sheets_per_package']))
+              +'&'+'full_packages='+encodeURIComponent(get_value(form.elements['full_packages']))
+              +'&'+'doublesided='+encodeURIComponent(get_value(form.elements['doublesided']))
+              +'&'+'price='+encodeURIComponent(get_value(form.elements['CustomStockPrice']))
 
               );
       } else {
