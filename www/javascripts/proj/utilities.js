1f1cf10d5aa05f30b1f3b06993b4f1dba5e9fbcd
Isaac Connor <iconnor@connortechnology.com>
2025-03-04 22:47:52 -0500

    Fix growtable to not kill inputs oninput

--- www/javascripts/proj/utilities.js
+++ www/javascripts/proj/utilities.js
@@ -202,57 +202,66 @@ function details_icon (ico_container, ico_pos, ico_text, ico_cursor) {
 // will automatically remove itself from what was the last row after it add a
 // new one.
 function growTable (last_row) {
-    if (! document.getElementById) return;
+  if (! document.getElementById) return;
 
-    while (last_row.nodeName != 'TR') last_row = last_row.parentNode;
+  console.log("growTable");
+  while (last_row.nodeName != 'TR') {
+    console.log('Finding tr', last_row, last_row.parentNode);
+    last_row = last_row.parentNode;
+  }
 
-    var tbody = last_row.parentNode;
-    var next_row = last_row.cloneNode(true); // Copy entire row.
+  var tbody = last_row.parentNode;
+  var next_row = last_row.cloneNode(true); // Copy entire row.
 
-    // TEMPORARY: This should be replaced by a callback.
-    var inputs = next_row.getElementsByTagName('input');
-    var pattern = /[0-9]+$/;
-    for (var i=0; i < inputs.length; i++) {
-        var input = inputs[i];
-        var n = parseInt(input.name.match(pattern));
+  // TEMPORARY: This should be replaced by a callback.
+  var inputs = next_row.getElementsByTagName('input');
+  var pattern = /[0-9]+$/;
+  for (var i=0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var n = parseInt(input.name.match(pattern));
 
-        if (n == NaN || n <= 0) {
-            alert('Invalid field name: '+input.name);
-            input.name = '';
-        }
-
-		input.name = input.name.replace(pattern, ++n);
-		input.id   = input.id.replace(pattern, n);
-        // Clear any values from the old record.
-        input.value = '';
+    if (n == NaN || n <= 0) {
+      alert('Invalid field name: '+input.name);
+      input.name = '';
     }
 
-    // TEMPORARY: This should be replaced by a callback.
-    var inputs = next_row.getElementsByTagName('select');
-    var pattern = /[0-9]+$/;
-    for (var i=0; i < inputs.length; i++) {
-        var input = inputs[i];
-        var n = parseInt(input.name.match(pattern));
+    input.name = input.name.replace(pattern, ++n);
+    input.id   = input.id.replace(pattern, n);
+    // Clear any values from the old record.
+    input.value = '';
+  }
 
-        if (n == NaN || n <= 0) {
-            alert('Invalid field name: '+input.name);
-            input.name = '';
-        }
+  // TEMPORARY: This should be replaced by a callback.
+  var inputs = next_row.getElementsByTagName('select');
+  var pattern = /[0-9]+$/;
+  for (var i=0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var n = parseInt(input.name.match(pattern));
 
-		input.name = input.name.replace(pattern, ++n);
-		input.id   = input.id.replace(pattern, n);
-        // Clear any values from the old record.
-        input.value = '';
+    if (n == NaN || n <= 0) {
+      alert('Invalid field name: '+input.name);
+      input.name = '';
     }
 
-    tbody.appendChild(next_row);
-    // Remove the growtbody event from all inputs in what was the last_row.
-    var inputs = last_row.getElementsByTagName('input');
-    for (var i=0; i < inputs.length; i++) {
-        inputs[i].onkeypress = function () {};
-        inputs[i].oninput = function () {};
+    input.name = input.name.replace(pattern, ++n);
+    input.id   = input.id.replace(pattern, n);
+    // Clear any values from the old record.
+    input.value = '';
+  }
+
+  tbody.appendChild(next_row);
+  // Remove the growtbody event from all inputs in what was the last_row.
+  var inputs = last_row.getElementsByTagName('input');
+  for (var i=0; i < inputs.length; i++) {
+    inputs[i].onkeypress = function () {};
+    console.log(inputs[i].oninput, growTable);
+    if (inputs[i].oninput == growTable) {
+      console.log('clearing');
+      inputs[i].oninput = function () {};
     }
-    // TODO: Use the event model TODO: Remove event from selects and textareas.
+  }
+  // TODO: Use the event model TODO: Remove event from selects and textareas.
+  console.log('done');
 }
 
 
