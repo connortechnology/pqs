9feada8d8822ec6af6297369f11113838010b268
Isaac Connor <iconnor@connortechnology.com>
2025-04-15 16:19:00 -0400

    Add Folds

---
+++ db/Fold_Specifications.sql
@@ -0,0 +1,105 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: fold_specifications; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.fold_specifications VALUES (1, 1, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (2, 2, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (3, 3, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (4, 4, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (5, 5, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (6, 6, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (7, 7, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (8, 8, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (9, 9, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (10, 10, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (11, 11, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (12, 12, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (13, 13, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (14, 14, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (15, 15, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (16, 16, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (17, 17, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (18, 18, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (19, 19, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (20, 20, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (21, 21, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (22, 22, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (23, 23, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (24, 24, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (25, 25, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (26, 26, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (27, 27, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (28, 28, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (29, 29, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (30, 30, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (31, 31, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (32, 32, NULL, NULL, 'gsm', 2000, false);
+INSERT INTO public.fold_specifications VALUES (33, 33, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (34, 34, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (35, 35, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (36, 36, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (37, 37, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (38, 38, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (39, 39, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (40, 40, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (41, 41, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (42, 42, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (43, 43, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (44, 44, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (45, 45, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (46, 46, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (47, 47, NULL, NULL, 'gsm', 10000, false);
+INSERT INTO public.fold_specifications VALUES (48, 48, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (49, 49, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (50, 50, NULL, NULL, 'gsm', 3000, false);
+INSERT INTO public.fold_specifications VALUES (51, 51, NULL, NULL, 'gsm', 2500, false);
+INSERT INTO public.fold_specifications VALUES (52, 52, NULL, NULL, 'gsm', 5000, false);
+INSERT INTO public.fold_specifications VALUES (53, 53, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (54, 54, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (55, 55, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (56, 56, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (57, 57, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (58, 58, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (59, 59, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (60, 60, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (61, 61, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (62, 62, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (63, 63, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (64, 64, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (65, 65, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (66, 66, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (67, 67, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (68, 68, NULL, NULL, 'gsm', 1000, false);
+INSERT INTO public.fold_specifications VALUES (69, 69, NULL, NULL, 'gsm', 0, false);
+INSERT INTO public.fold_specifications VALUES (70, 70, NULL, NULL, 'gsm', 1000, false);
+
+
+--
+-- Name: fold_specifications_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.fold_specifications_id_seq', 70, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
