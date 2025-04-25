f3feca7959df5ba2ce49ebaf1c059a4868db4ba9
Isaac Connor <iconnor@connortechnology.com>
2025-04-25 19:05:52 -0400

    Remove disconnects that are now handled by cleanup

--- perl/eprint/Service/Handler.pm
+++ perl/eprint/Service/Handler.pm
@@ -46,60 +46,60 @@ use constant PROJECT_VIEW_PAGE  => '/main/proj/proj_view.html';
 sub handler {
   $request = shift;
 	$request->push_handlers(PerlCleanupHandler => \&openprint::www::cleanup);
-    $r = Apache2::Request->new($request,
-        POST_MAX        => 8096,
-        DISABLE_UPLOADS => 1,
-    );
-    my $variable = \%variable;
+  $r = Apache2::Request->new($request,
+    POST_MAX        => 8096,
+    DISABLE_UPLOADS => 1,
+  );
+  my $variable = \%variable;
 
-    # Process the request params.
-    $r->parse;
+  # Process the request params.
+  $r->parse;
 
-    session::r($r);
-    session::log($r->log);
-    $log = $r->log;
+  session::r($r);
+  session::log($r->log);
+  $log = $r->log;
 
-    if (!( $r->method_number == M_GET || $r->method_number == M_POST)) {
-      print STDERR "Invalid method ".$r->method_number." get:".M_GET.' post:'.M_POST."\n";
-      return HTTP_METHOD_NOT_ALLOWED;
-    }
-    $dbh = PQS::DB->connect($r);  # TODO Use RO session for GETs
-    session::dbh($dbh);
+  if (!( $r->method_number == M_GET || $r->method_number == M_POST)) {
+    print STDERR "Invalid method ".$r->method_number." get:".M_GET.' post:'.M_POST."\n";
+    return HTTP_METHOD_NOT_ALLOWED;
+  }
+  $dbh = PQS::DB->connect($r);  # TODO Use RO session for GETs
+  session::dbh($dbh);
 
-    # If the customer isn't valid and logged in, they can't use us.
-    my $cookie = misc::get_cookie();
-    if (!defined $cookie || $cookie eq '') {
-      $r->headers_out->set(Location => '/main/account/account_login.html');
-      $r->status(Apache2::Const::REDIRECT); #302
-      return Apache2::Const::OK;
-    }
+  # If the customer isn't valid and logged in, they can't use us.
+  my $cookie = misc::get_cookie();
+  if (!defined $cookie || $cookie eq '') {
+    $r->headers_out->set(Location => '/main/account/account_login.html');
+    $r->status(Apache2::Const::REDIRECT); #302
+    return Apache2::Const::OK;
+  }
 
-    #%openprint::param = %{$variable->{param}} = map {$_ => $r->param($_)} $r->param();
-    # Here we copy the param data into a hash that is sligthly more useful to use.  Wish we didn't have to do this.
-    foreach my $key ( $r->param ) {
-      my @values = $r->param($key);
-      $key = substr($key,0,-2) if (substr($key, -2, 2) eq '[]');
-      if ( @values > 1 ) {
-        $param{$key} = \@values;
-        #$log->debug("Parameter $key is ARRAY(" . join(',',@{$param{$key}}) . ')' );
+  #%openprint::param = %{$variable->{param}} = map {$_ => $r->param($_)} $r->param();
+  # Here we copy the param data into a hash that is sligthly more useful to use.  Wish we didn't have to do this.
+  foreach my $key ( $r->param ) {
+    my @values = $r->param($key);
+    $key = substr($key,0,-2) if (substr($key, -2, 2) eq '[]');
+    if ( @values > 1 ) {
+      $param{$key} = \@values;
+      #$log->debug("Parameter $key is ARRAY(" . join(',',@{$param{$key}}) . ')' );
+    } else {
+      my $x = $values[0];
+      if (utf8::decode($x)) {
+        $param{$key} = $x;
       } else {
-        my $x = $values[0];
-        if (utf8::decode($x)) {
-          $param{$key} = $x;
-        } else {
-          $param{$key} = $values[0];
-        }
-        #$log->debug("Parameter $key is (" . $param{$key} . ") ref: " . ref $param{$key} );
-      } # end if
-    } # end foreach
-    foreach my $key ( sort keys %param ) {
-      if ( ref $param{$key} eq 'ARRAY' ) {
-        $log->debug('Parameter '.$key.' is ARRAY(' . join(',', @{$param{$key}}) . ')');
-      } else {
-        $log->debug('Parameter '.$key.' is ('.$param{$key}.')');# . (utf8::is_utf8($param{$key})||0) );
-        #$log->debug("Parameter $key is (" . $param{$key} . ")" . (utf8::is_utf8($param{$key})||0) );
-      } # end if
-    } # end foreach
+        $param{$key} = $values[0];
+      }
+      #$log->debug("Parameter $key is (" . $param{$key} . ") ref: " . ref $param{$key} );
+    } # end if
+  } # end foreach
+  foreach my $key ( sort keys %param ) {
+    if ( ref $param{$key} eq 'ARRAY' ) {
+      $log->debug('Parameter '.$key.' is ARRAY(' . join(',', @{$param{$key}}) . ')');
+    } else {
+      $log->debug('Parameter '.$key.' is ('.$param{$key}.')');# . (utf8::is_utf8($param{$key})||0) );
+      #$log->debug("Parameter $key is (" . $param{$key} . ")" . (utf8::is_utf8($param{$key})||0) );
+    } # end if
+  } # end foreach
   openprint::configuration::init( $r->dir_config() );
   openprint::session_init();
 
@@ -107,96 +107,85 @@ sub handler {
   eprint::material::init_cache();
   eprint::equipment::init_cache();
 
-    my $customer_id = eprint::login::get_login_info( # Populates $variable
-        $r->log, $dbh, $cookie, $variable, 'C'
-    );
-    if (!$customer_id) {
-      $r->headers_out->set(Location => '/main/account/account_login.html');
-      $r->status(Apache2::Const::REDIRECT); #302
-      $dbh->disconnect;
-      return Apache2::Const::OK;
-    }
+  my $customer_id = eprint::login::get_login_info( # Populates $variable
+    $r->log, $dbh, $cookie, $variable, 'C'
+  );
+  if (!$customer_id) {
+    $r->headers_out->set(Location => '/main/account/account_login.html');
+    $r->status(Apache2::Const::REDIRECT); #302
+    return Apache2::Const::OK;
+  }
 
-    # Determine if we're a known service for processing.
-    my $service = uri_to_service($r, $dbh);
-    if (!$service) {
-      $openprint::log->error("Service not found");
-      $dbh->disconnect and return NOT_FOUND;
-    }
+  # Determine if we're a known service for processing.
+  my $service = uri_to_service($r, $dbh);
+  if (!$service) {
+    $openprint::log->error("Service not found");
+  }
 
-	#map { print STDERR "HAVE PARAM: $_ = " . $r->param($_) . " \n"; } $r->param();
+  #map { print STDERR "HAVE PARAM: $_ = " . $r->param($_) . " \n"; } $r->param();
 
-    my $pid = $r->param('pid') ? $r->param('pid') : $r->param('project_id');
-    my $sid = $r->param('sid') ? $r->param('sid') : $r->param('service_id');
-    if (!($pid and $sid)) {
-      $r->headers_out->set(Location => '/main/proj/proj_hist.html');
-      $r->status(Apache2::Const::REDIRECT); #302
-      $dbh->disconnect;
-      return Apache2::Const::OK;
-    }
+  my $pid = $r->param('pid') ? $r->param('pid') : $r->param('project_id');
+  my $sid = $r->param('sid') ? $r->param('sid') : $r->param('service_id');
+  if (!($pid and $sid)) {
+    $r->headers_out->set(Location => '/main/proj/proj_hist.html');
+    $r->status(Apache2::Const::REDIRECT); #302
+    return Apache2::Const::OK;
+  }
 
-    # Make sure the service exists in the project.
-    unless ($dbh->selectrow_array(q{
-                SELECT true 
-                FROM tbl_project_contents 
-                WHERE lngprojectindex = ?
-                  AND lngserviceindex = ?
-            }, undef, $pid, $sid))
-    {
-print STDERR "Service not found for $pid/$sid\n";
-if ($dbh->selectrow_array('SELECT true from tbl_projects WHERE lngprojectindex=?', undef, $pid)) {
+  # Make sure the service exists in the project.
+  unless ($dbh->selectrow_array(q{
+      SELECT true 
+      FROM tbl_project_contents 
+      WHERE lngprojectindex = ?
+      AND lngserviceindex = ?
+      }, undef, $pid, $sid))
+  {
+    print STDERR "Service not found for $pid/$sid\n";
+    if ($dbh->selectrow_array('SELECT true from tbl_projects WHERE lngprojectindex=?', undef, $pid)) {
       $r->headers_out->set(Location => '/main/proj/proj_view.html?pid='.$pid);
       $r->status(Apache2::Const::REDIRECT); #302
-      $dbh->disconnect;
       return Apache2::Const::OK;
     }
 
-        $dbh->disconnect;
-        return NOT_FOUND;
+    return NOT_FOUND;
+  }
+
+  # To view/edit this service the user must be allowed to edit the project
+  # as a whole and if the project has locked service (fixed price) the
+  # service must be in the allowed list (or the user is staff).
+  my $allowed = allowed_services($dbh, $pid);
+
+  if (! project_allowed($dbh, $pid, $variable)
+    || (    has_locked_services($dbh, $pid) 
+      && !exists $allowed->{ $service->{id} } 
+      && !$variable->{is_staff} ) )
+  {
+    print STDERR "Forbidden for $pid\n";
+    return FORBIDDEN; 
+  }
+
+  # Generate and send the response.
+  eval { response($r, $dbh, $variable, $pid, $sid, $service) };
+
+  # If there's a problem with the request, log the error and optionally send
+  # a stacktrace if we're in debugging mode.
+  if ($@) {
+    my $err = $@;
+
+    $log->error($err);
+
+    if (DEBUG) {
+      require Error::StackTrace;
+      $r->status(SERVER_ERROR);
+      $r->content_type('text/html');
+      print Error::StackTrace::trace($r, $err);
+      return OK;
     }
 
-    # To view/edit this service the user must be allowed to edit the project
-    # as a whole and if the project has locked service (fixed price) the
-    # service must be in the allowed list (or the user is staff).
-    my $allowed = allowed_services($dbh, $pid);
+    return SERVER_ERROR;
+  }
 
-    if (! project_allowed($dbh, $pid, $variable)
-        || (    has_locked_services($dbh, $pid) 
-             && !exists $allowed->{ $service->{id} } 
-             && !$variable->{is_staff} ) )
-    {
-print STDERR "Forbidden for $pid\n";
-        $dbh->disconnect;
-        return FORBIDDEN; 
-    }
-    
-    # Generate and send the response.
-    eval { response($r, $dbh, $variable, $pid, $sid, $service) };
-
-    # If there's a problem with the request, log the error and optionally send
-    # a stacktrace if we're in debugging mode.
-    if ($@) {
-        my $err = $@;
-
-        #$dbh->rollback;
-        $dbh->disconnect;
-    
-        $log->error($err);
-                
-        if (DEBUG) {
-            require Error::StackTrace;
-            $r->status(SERVER_ERROR);
-            $r->content_type('text/html');
-            print Error::StackTrace::trace($r, $err);
-            return OK;
-        }
-      
-        return SERVER_ERROR;
-    }
-  
-    $dbh->disconnect if $dbh;
-
-    return OK;
+  return OK;
 }
 
 # Map the uri to a valid service type, return a hash ref of it's attributes.
