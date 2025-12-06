96d535de1537007bde3d6f7d39973b032a902eea
Isaac Connor <iconnor@connortechnology.com>
2025-12-06 18:17:52 -0500

    UPgrade to openprint version of state/provinces/countries

--- perl/provinces.pm
+++ perl/provinces.pm
@@ -1,20 +1,25 @@
-package provinces;
 use strict;
+package provinces;
 
-our @provinces = (
-    AB => 'Alberta',
-    BC => 'British Columbia',
-    MB => 'Manitoba',
-    NB => 'New Brunswick',
-    NF => 'Newfoundland',
-    NT => 'Northwest Territories',
-    NS => 'Nova Scotia',
-    NU => 'Nunavut',
-    ON => 'Ontario',
-    PE => 'Prince Edward Island',
-    QC => 'Quebec',
-    SK => 'Saskatchewan',
-    YT => 'Yukon'
+use vars qw( @provinces %provinces );
+
+@provinces = (
+	'AB', 'Alberta',
+	'BC', 'British Columbia',
+	'MB', 'Manitoba',
+	'NB', 'New Brunswick',
+	'NF', 'Newfoundland',
+	'NT', 'Northwest Territories',
+	'NS', 'Nova Scotia',
+	'NU', 'Nunavut',
+	'ON', 'Ontario',
+	'PE', 'Prince Edward Island',
+	'QC', 'Quebec',
+	'SK', 'Saskatchewan',
+	'YT', 'Yukon'
 );
 
+%provinces = @provinces;
+
 1;
+__END__
